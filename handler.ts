5c7771a201f35dbacb971963d089726af5f1fcdf
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-12-08 20:18:14 +0900

    add date debug

--- handler.ts
+++ handler.ts
@@ -18,7 +18,11 @@ const {
 const slackClient = new WebClient(SLACK_API_TOKEN);
 
 export const main: APIGatewayProxyHandler = async () => {
-  console.log(new Date());
+  const d = new Date();
+  console.log(d);
+  console.log(d.getDate());
+  console.log(d.getHours());
+  console.log(d.getMinutes());
 
   const browser = await puppeteer.launch({
     args,
